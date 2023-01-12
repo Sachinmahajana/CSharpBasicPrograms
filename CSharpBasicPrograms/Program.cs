109aab71d505bb064756c105421ae191ce8d298d
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-12 18:00:08 +0530

    [Sachin] Adding Addition functionality

--- CSharpBasicPrograms/Program.cs
+++ CSharpBasicPrograms/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Csharp basic programs");
+         Console.WriteLine("Welcome to Csharp basic programs");
+
+            
+            
+         Console.WriteLine("Please enter first number");
+            int first=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int  second=Convert.ToInt32(Console.ReadLine());
+
+            Calculator calculator = new Calculator(first,second);
+            calculator.Addition();
             Console.ReadLine();
         }
     }
