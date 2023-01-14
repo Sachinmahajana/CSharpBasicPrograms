1afa6deb67406a19729744d375c18c77459df441
Sachinmahajana <120769632+Sachinmahajana@users.noreply.github.com>
2023-01-14 21:22:00 +0530

    [Sachin]  Adding calculator functionality

--- CSharpBasicPrograms/Program.cs
+++ CSharpBasicPrograms/Program.cs
@@ -2,12 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+      public static void Main(string[] args)
         {
          Console.WriteLine("Welcome to Csharp basic programs");
-
-            
-            
+      
          Console.WriteLine("Please enter first number");
             int first=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter second number");
